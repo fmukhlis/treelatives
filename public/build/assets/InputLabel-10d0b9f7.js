39ed846c9d62260216cb4623f9fc5117e84d840e
Fajar Mukhlis Imananda <cipherporu9@gmail.com>
2023-04-07 23:08:06 +0700

    First commit

---
+++ public/build/assets/InputLabel-10d0b9f7.js
@@ -0,0 +1,1 @@
+import{a as m}from"./app-77394a0d.js";function o({value:t,className:e="",children:a,...r}){return m("label",{...r,className:"block font-medium text-sm text-gray-700 "+e,children:t||a})}export{o as I};
